4ed13a8b1ac9ec5bc73702b0e6da2063c1ece2f6
Miki <sableangle@gmail.com>
2018-11-11 13:43:53 +0800

    暴風雪

---
+++ Assets/AI_Storm.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class AI_Storm : MonoBehaviour
+{
+    public Renderer stormmaterial;
+    public GameObject particle;
+
+    float alpha = 0;
+    public float speed = 2;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        alpha = 0;
+        stormmaterial.material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f, alpha));
+        yield return Yielders.GetWaitForSeconds(Random.Range(8, 15));
+        particle.SetActive(true);
+        alpha = 0.1f;
+        while (alpha > 0)
+        {
+            alpha += Time.deltaTime * speed;
+            stormmaterial.material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f, alpha));
+            yield return null;
+        }
+
+        particle.SetActive(false);
+
+		yield return Start();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            alpha -= Time.deltaTime * speed * 1.5f;
+			
+        }
+    }
+
+
+}
